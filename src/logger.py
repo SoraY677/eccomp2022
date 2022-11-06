d54c29d4c21aa46b9ed11f9d165307920db3d5c9
SoraY677 <SoraY677@outlook.com>
2022-11-06 14:41:26 +0900

    ログローテション

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,12 @@
 import os
 import logging
+import datetime
 from .util import root_path as rp
 
-WRITE_FILE_PATH = os.path.join(rp.get_root_path(), 'app.log')
+_dt =  datetime.datetime.today()
+_time = f'{_dt.year}-{_dt.month}-{_dt.day}-{_dt.hour}-{_dt.minute}-{_dt.second}'
+WRITE_FILE_PATH = os.path.join(rp.get_root_path(), 'log', _time + '.log')
+
 
 def set_log_write_file():
   logging.basicConfig(filename=WRITE_FILE_PATH, level="DEBUG")
