6e82910165b9d5863671e9e4d358b3fc01cda59c
SoraY677 <SoraY677@outlook.com>
2022-11-06 12:54:11 +0900

    処理を少し軽減

--- src/config.py
+++ src/config.py
@@ -5,13 +5,14 @@ from util import root_path as rp
 _config = {}
 
 def _read_config():
+  global _config
   root_path = rp.get_root_path()
   with open(os.path.join(root_path + 'config.yml')) as file:
-    return yaml.safe_load(file)
+    _config = yaml.safe_load(file)
+_read_config()
 
 def get_config():
   global _config
-  _config = _read_config()
   return _config
 
 if __name__ == '__main__':
