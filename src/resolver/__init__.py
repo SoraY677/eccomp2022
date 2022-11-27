5f6a1ede487338f0778a4974a456a4e88b2510e4
SoraY677 <SoraY677@outlook.com>
2022-11-27 15:32:26 +0900

    set_configを追加

--- src/resolver/__init__.py
+++ src/resolver/__init__.py
@@ -2,12 +2,14 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from resolver_conig import set_config
 from generate import random_generate as first_generate
 from crossover import cross,union
 from selecter import select
 from mutation import mutate
 
 __all__ = [
+  'set_config',
   'first_generate',
   'cross',
   'union',
