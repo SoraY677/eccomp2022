1cd1a1880838851b32a961ff40e5095d11d7fd9b
SoraY677 <SoraY677@outlook.com>
2022-11-27 16:26:42 +0900

    graph出すところを変える

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,6 @@ import command
 from exec import run
 import submit
 import store
-import graph_plotter
 
 def process() -> None:
   '''
@@ -23,14 +22,13 @@ def process() -> None:
   logger.init(id, is_clear)
   submit.init(id, is_clear)
   store_map = store.init(id, is_clear)
-  graph_plotter.init(id, is_clear)
 
   logger.log_info(f'[mode] {mode}')
 
   dt_start =  datetime.datetime.today()
   logger.log_info(f'[start]{dt_start.strftime("%Y-%m-%d %H:%M:%S")}')
 
-  run(mode, optimize_mode,optimize_number, store_map)
+  run(mode, optimize_mode, optimize_number, id, store_map)
 
   dt_end = datetime.datetime.today()
   logger.log_info(f'[end]{dt_end.strftime("%Y-%m-%d %H:%M:%S")}')
@@ -41,6 +39,6 @@ def process() -> None:
   logger.log_info(f'[process result]{dt_dif.days}d {dt_dif.seconds}.{dt_dif.microseconds}s')
 
   best_score_list = submit.get_best_score_list()
-  graph_plotter.plot_best_score_list(best_score_list)
+  logger.log_info(f'[best score list] {best_score_list}')
   
 __all__ = ['process']
